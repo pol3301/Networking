425af7eb06b39e2de85ca27e7cb248031a06842d
pol <masterlydata509@gmail.com>
2026-07-23 01:02:34 -0600

    No longer needs port as input field

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,85 @@
-use std::{net::SocketAddr, time::Duration};
+use std::{
+    net::{IpAddr, SocketAddr},
+    time::Duration,
+};
 
 use tokio::{
     net::UdpSocket,
     time::{self, interval},
 };
 
-async fn punch_hole(socket: &UdpSocket, peer_addr: SocketAddr) -> Result<(), &'static str> {
+const PORTS_LIST: [&str; 5] = ["32432", "24325", "24377", "25379", "36727"];
+
+async fn punch_hole(socket: &UdpSocket, peer_ip: IpAddr) -> Result<SocketAddr, &'static str> {
     let mut ticker = interval(Duration::from_millis(200));
     let mut buf = [0; 1024];
 
     let punch: &[u8] = b"PUNCH";
     let ack: &[u8] = b"ACK";
 
+    let peer_addr_list: [SocketAddr; 5] =
+        PORTS_LIST.map(|port| SocketAddr::new(peer_ip, port.parse::<u16>().unwrap()));
+
     let mut current_strategy = punch;
+
+    let mut peer_addr: Option<SocketAddr> = None;
 
     let punch_attempt = time::timeout(Duration::from_mins(1), async {
         loop {
             tokio::select! {
                 _ = ticker.tick() => {
-                    let _ = socket.send_to(current_strategy, peer_addr).await;
+                match peer_addr {
+                    Some(addr) => {let _ = socket.send_to(current_strategy, addr).await;},
+                    None => {
+                        for addr in peer_addr_list {
+                            let _ = socket.send_to(current_strategy, addr).await;
+                        }
+                    },
+                }
                 }
 
                 result = socket.recv_from(&mut buf) => {
-                    if let Ok((len, addr)) = result &&
-                        addr == peer_addr {
+                    if let Ok((len, connection_addr)) = result {
+                        if let Some(addr) = peer_addr {
+                            if addr == connection_addr {
+                                peer_addr = Some(addr);
+                                match &buf[..len] {
+                                    b"PUNCH" => {
+                                        current_strategy = ack;
+                                        let _ = socket.send_to(current_strategy, addr).await;
+                                    },
 
-                        match &buf[..len] {
-                            b"PUNCH" => {
-                                current_strategy = ack;
-                                _ = socket.send_to(current_strategy, peer_addr)
-                            },
+                                    b"ACK" => {
+                                        for _ in 0..3 {
+                                            let _ = socket.send_to(b"ACK", addr).await;
+                                        }
 
-                            b"ACK" => {
-                                for _ in 0..3 {
-                                    let _ = socket.send_to(b"ACK", peer_addr).await;
+                                        return peer_addr.unwrap();
+                                    },
+                                    _ => {},
                                 }
+                            }
+                        } else {
+                            for addr in peer_addr_list {
+                                if addr == connection_addr {
+                                    peer_addr = Some(addr);
+                                    match &buf[..len] {
+                                        b"PUNCH" => {
+                                            current_strategy = ack;
+                                            let _ = socket.send_to(current_strategy, addr).await;
+                                        },
 
-                                return;
-                            },
-                            _ => {},
+                                        b"ACK" => {
+                                            for _ in 0..3 {
+                                                let _ = socket.send_to(b"ACK", addr).await;
+                                            }
+
+                                            return peer_addr.unwrap();
+                                        },
+                                        _ => {},
+                                    }
+                                }
+                            }
                         }
                     }
                 }
@@ -48,19 +89,17 @@ async fn punch_hole(socket: &UdpSocket, peer_addr: SocketAddr) -> Result<(), &'s
     .await;
 
     match punch_attempt {
-        Ok(_) => Ok(()),
+        Ok(peer_addr) => Ok(peer_addr),
         Err(_) => Err("Failed to punch a hole in the given time"),
     }
 }
-
-const PORTS_LIST: [&str; 5] = ["32432", "24325", "24377", "25379", "36727"];
 
 #[tokio::main]
 async fn main() {
     let args: Vec<String> = std::env::args().collect();
 
-    if args.len() != 3 {
-        eprintln!("Wrong usage: ./program peer_addr port");
+    if args.len() != 2 {
+        eprintln!("Wrong usage: ./program peer_addr");
         return;
     }
 
@@ -81,14 +120,8 @@ async fn main() {
         .parse::<std::net::IpAddr>()
         .unwrap_or_else(|_| panic!("Could not parse address {}", args[1]));
 
-    let peer_port = args[2]
-        .parse::<u16>()
-        .unwrap_or_else(|_| panic!("Could not parse port {}", args[2]));
-
-    let peer_addr = SocketAddr::new(peer_ip, peer_port);
-
-    match punch_hole(&socket, peer_addr).await {
-        Ok(_) => println!("Punched a hole!"),
+    match punch_hole(&socket, peer_ip).await {
+        Ok(addr) => println!("Punched a hole! Peer's port is: {}", addr.port()),
         Err(e) => eprintln!("{}", e),
     }
 }
